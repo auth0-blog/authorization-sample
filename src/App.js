6b56db1fd4659512adca910fae7fe507c39093e5
Bruno Krebs <bruno.krebs@auth0.com>
2019-02-10 17:39:54 -0200

    adding Ant;

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
+import { Layout, Menu, Breadcrumb } from 'antd';
 import React, { Component } from 'react';
 import { Router, Link } from '@reach/router';
 import Grades from './components/Grades';
 import Home from './components/Home';
 
+import 'antd/dist/antd.css';
+
+const { Header, Content, Footer } = Layout;
+
 class App extends Component {
   render() {
     return (
-      <>
-        <div>
-          <Link to="/">Home </Link>
-          <Link to="grades"> Dashboard</Link>
-        </div>
-        <Router>
-          <Home path="/" />
-          <Grades path="grades" />
-        </Router>
-      </>
+      <Layout className="layout">
+        <Header>
+          <div className="logo">
+            React App
+          </div>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1"><Link to="/">Home</Link></Menu.Item>
+            <Menu.Item key="2"><Link to="grades"> Dashboard</Link></Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '15px 50px' }}>
+          <Router>
+            <Home path="/" />
+            <Grades path="grades" />
+          </Router>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Developed with ❤ by Auth0
+        </Footer>
+      </Layout>
     );
   }
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React, {useState} from 'react';
+
+function NavBar() {
+  const [authStatus, setAuthStatus] = useState(authStatus, setAuthStatus);
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default NavBar;
